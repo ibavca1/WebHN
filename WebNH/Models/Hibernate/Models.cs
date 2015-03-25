fda68263610b6c884943d65974b68003f81beb44
Белоногов А.В. <it.analyst.programmer4@sparural.ru>
2015-03-25 16:00:56 +0500

    Добавил модели в Hibernate

---
+++ WebNH/Models/Hibernate/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebNH.Models.Hibernate
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public DepType Type { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum DepType
+    {
+        Oracle,
+        MsSql,
+        Windows
+    }
+
+    public enum ProxyType
+    {
+        Sock4,
+        Sock4a,
+        Sock5,
+        Http,
+        Https
+    }
+}
